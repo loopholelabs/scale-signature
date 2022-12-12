d2d85ced7b84468707b23eba7d8774214f55625b
Daniel Phillips <daniel@loopholelabs.io>
2022-12-12 10:00:42 -0600

    Add rust pub traits to match go interfaces

--- signature.rs
+++ signature.rs
@@ -1,20 +1,19 @@
-TODO // document like go Interfaces, rm/make private all but read/write/error
+use scale_signature_http::{HttpResponse};
+use std::io::Error;
+use std::io::Cursor;
+
 pub trait RuntimeContext {
-    fn new(self) -> Self;
     fn read(&mut self) -> HttpContext;
     fn write(&self) -> Vec<u8>;
     fn error(&self, err: std::io::Error) -> Vec<u8>;
     fn generated(&self) -> &HttpContext;
+    fn new(self) -> Self;
 }
 
 pub trait GuestContext {
     fn from_read_buffer(self, read_buff: &mut Cursor<&mut Vec<u8>>) -> Result<HttpContext, Error> ;
     fn to_write_buffer(self) -> Result<(u32, u32), Error>;
     fn error_write_buffer(self, error: &str) -> (u32, u32);
-
-TODO // document like go Interfaces, rm/make private all below
-    fn new() -> Self;
     fn next(self) -> Self;
-    fn request(&mut self) -> &mut HttpRequest;
-    fn response(&mut self) -> &mut HttpResponse;
+    fn new() -> Self;
 }
