09779b54d3fe5ddb39a172296340ca80f0c639b0
Daniel Phillips <daniel@loopholelabs.io>
2023-01-24 14:43:17 -0600

    Chg: make sig trait match other interfaces

--- signature.rs
+++ signature.rs
@@ -1,20 +1,14 @@
 use std::io::Error;
 use std::io::Cursor;
 
-use scale_signature_http::http_signature::{HttpContext};
-
 pub trait RuntimeContext {
-    fn read(&mut self) -> HttpContext;
+    fn read(&mut self) -> Error;
     fn write(&self) -> Vec<u8>;
-    fn error(&self, err: std::io::Error) -> Vec<u8>;
-    fn generated(&self) -> &HttpContext;
-    fn new(self) -> Self;
+    fn error(&self, err: Error) -> Vec<u8>;
 }
 
 pub trait GuestContext {
-    fn from_read_buffer(self, read_buff: &mut Cursor<&mut Vec<u8>>) -> Result<HttpContext, Error> ;
     fn to_write_buffer(self) -> Result<(u32, u32), Error>;
     fn error_write_buffer(self, error: &str) -> (u32, u32);
-    fn next(self) -> Self;
-    fn new() -> Self;
+    fn from_read_buffer(self, read_buff: &mut Cursor<&mut Vec<u8>>) -> Error;
 }
