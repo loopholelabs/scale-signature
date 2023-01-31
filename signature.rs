a7b6e0984cdc6f5a916a04f29b855f185fc66f9b
Shivansh Vij <shivanshvij@loopholelabs.io>
2023-01-31 18:50:22 -0500

    Updating signature to match the scale-signature-http semantics

--- signature.rs
+++ signature.rs
@@ -11,13 +11,13 @@ pub trait Context {
 }
 
 pub trait RuntimeContext {
-    fn read(&mut self) -> Error;
+    fn read(&mut self) -> Option<Error>;
     fn write(&self) -> Vec<u8>;
-    fn error(&self, err: Error) -> Vec<u8>;
+    fn error(&self, err: &str) -> Vec<u8>;
 }
 
 pub trait GuestContext {
-    fn to_write_buffer(self) -> (u32, u32);
-    fn error_write_buffer(self, error: &str) -> (u32, u32);
-    unsafe fn from_read_buffer(self) -> Option<Error>;
+    unsafe fn to_write_buffer(&mut self) -> (u32, u32);
+    unsafe fn error_write_buffer(&mut self, error: &str) -> (u32, u32);
+    unsafe fn from_read_buffer(&mut self) -> Option<Error>;
 }
