b5c5d8d4a4fa293fa3d1abe105464b19c9310b6c
Shivansh Vij <shivanshvij@loopholelabs.io>
2023-06-06 15:08:48 -0700

    Partially complete test output for typescript

--- integration/typescript_tests/index.test.ts
+++ integration/typescript_tests/index.test.ts
@@ -133,6 +133,94 @@ test('test-output', () => {
     modelWithMultipleFieldsAccessor.int32Field = 42;
     modelWithMultipleFieldsAccessor.encode(modelWithMultipleFieldsAccessorEncoder);
     fs.writeFileSync('../binaries/model_with_multiple_fields_accessor.bin', modelWithMultipleFieldsAccessorEncoder.bytes, 'binary');
+
+    const modelWithMultipleFieldsAccessorAndDescriptionEncoder = new polyglot.Encoder();
+    const modelWithMultipleFieldsAccessorAndDescription = new generated.ModelWithMultipleFieldsAccessorAndDescription();
+    stringFieldValue = modelWithMultipleFieldsAccessorAndDescription.stringField;
+    expect(stringFieldValue).toEqual('DefaultValue');
+    modelWithMultipleFieldsAccessorAndDescription.stringField = 'hello world';
+    int32FieldValue = modelWithMultipleFieldsAccessorAndDescription.int32Field;
+    expect(int32FieldValue).toEqual(32);
+    modelWithMultipleFieldsAccessorAndDescription.int32Field = 42;
+    modelWithMultipleFieldsAccessorAndDescription.encode(modelWithMultipleFieldsAccessorAndDescriptionEncoder);
+    fs.writeFileSync('../binaries/model_with_multiple_fields_accessor_and_description.bin', modelWithMultipleFieldsAccessorAndDescriptionEncoder.bytes, 'binary');
+
+    const modelWithEmbeddedModelsEncoder = new polyglot.Encoder();
+    const modelWithEmbeddedModels = new generated.ModelWithEmbeddedModels();
+    expect(modelWithEmbeddedModels.embeddedEmptyModel).not.toBeNull();
+    expect(modelWithEmbeddedModels.embeddedEmptyModel).not.toBeUndefined();
+    expect(modelWithEmbeddedModels.embeddedModelArrayWithMultipleFieldsAccessor).not.toBeNull();
+    expect(modelWithEmbeddedModels.embeddedModelArrayWithMultipleFieldsAccessor).not.toBeUndefined();
+    expect(modelWithEmbeddedModels.embeddedModelArrayWithMultipleFieldsAccessor.length).toEqual(0);
+    modelWithEmbeddedModels.embeddedModelArrayWithMultipleFieldsAccessor.push(modelWithMultipleFieldsAccessor);
+    modelWithEmbeddedModels.encode(modelWithEmbeddedModelsEncoder);
+    fs.writeFileSync('../binaries/model_with_embedded_models.bin', modelWithEmbeddedModelsEncoder.bytes, 'binary');
+
+    const modelWithEmbeddedModelsAndDescriptionEncoder = new polyglot.Encoder();
+    const modelWithEmbeddedModelsAndDescription = new generated.ModelWithEmbeddedModelsAndDescription();
+    expect(modelWithEmbeddedModelsAndDescription.embeddedEmptyModel).not.toBeNull();
+    expect(modelWithEmbeddedModelsAndDescription.embeddedEmptyModel).not.toBeUndefined();
+    expect(modelWithEmbeddedModelsAndDescription.embeddedModelArrayWithMultipleFieldsAccessor).not.toBeNull();
+    expect(modelWithEmbeddedModelsAndDescription.embeddedModelArrayWithMultipleFieldsAccessor).not.toBeUndefined();
+    expect(modelWithEmbeddedModelsAndDescription.embeddedModelArrayWithMultipleFieldsAccessor.length).toEqual(0);
+    modelWithEmbeddedModelsAndDescription.embeddedModelArrayWithMultipleFieldsAccessor.push(modelWithMultipleFieldsAccessor);
+    modelWithEmbeddedModelsAndDescription.encode(modelWithEmbeddedModelsAndDescriptionEncoder);
+    fs.writeFileSync('../binaries/model_with_embedded_models_and_description.bin', modelWithEmbeddedModelsAndDescriptionEncoder.bytes, 'binary');
+
+    const modelWithEmbeddedModelsAccessorEncoder = new polyglot.Encoder();
+    const modelWithEmbeddedModelsAccessor = new generated.ModelWithEmbeddedModelsAccessor();
+    let embeddedModel = modelWithEmbeddedModelsAccessor.embeddedEmptyModel;
+    expect(embeddedModel).not.toBeNull();
+    expect(embeddedModel).not.toBeUndefined();
+    let embeddedModelArray = modelWithEmbeddedModelsAccessor.embeddedModelArrayWithMultipleFieldsAccessor;
+    expect(embeddedModelArray).not.toBeNull();
+    expect(embeddedModelArray).not.toBeUndefined();
+    expect(embeddedModelArray.length).toEqual(0);
+    modelWithEmbeddedModelsAccessor.embeddedModelArrayWithMultipleFieldsAccessor.push(modelWithMultipleFieldsAccessor);
+    modelWithEmbeddedModelsAccessor.encode(modelWithEmbeddedModelsAccessorEncoder);
+    fs.writeFileSync('../binaries/model_with_embedded_models_accessor.bin', modelWithEmbeddedModelsAccessorEncoder.bytes, 'binary');
+
+    const modelWithEmbeddedModelsAccessorAndDescriptionEncoder = new polyglot.Encoder();
+    const modelWithEmbeddedModelsAccessorAndDescription = new generated.ModelWithEmbeddedModelsAccessorAndDescription();
+    embeddedModel = modelWithEmbeddedModelsAccessorAndDescription.embeddedEmptyModel;
+    expect(embeddedModel).not.toBeNull();
+    expect(embeddedModel).not.toBeUndefined();
+    embeddedModelArray = modelWithEmbeddedModelsAccessorAndDescription.embeddedModelArrayWithMultipleFieldsAccessor;
+    expect(embeddedModelArray).not.toBeNull();
+    expect(embeddedModelArray).not.toBeUndefined();
+    expect(embeddedModelArray.length).toEqual(0);
+    modelWithEmbeddedModelsAccessorAndDescription.embeddedModelArrayWithMultipleFieldsAccessor.push(modelWithMultipleFieldsAccessor);
+    modelWithEmbeddedModelsAccessorAndDescription.encode(modelWithEmbeddedModelsAccessorAndDescriptionEncoder);
+    fs.writeFileSync('../binaries/model_with_embedded_models_accessor_and_description.bin', modelWithEmbeddedModelsAccessorAndDescriptionEncoder.bytes, 'binary');
+
+    const modelWithAllFieldTypesEncoder = new polyglot.Encoder();
+    const modelWithAllFieldTypes = new generated.ModelWithAllFieldTypes();
+    expect(modelWithAllFieldTypes.stringField).toEqual('DefaultValue');
+    modelWithAllFieldTypes.stringField = 'hello world';
+    expect(modelWithAllFieldTypes.stringArrayField.length).toEqual(0);
+    expect(modelWithAllFieldTypes.stringMapField).toEqual(new Map<string, string>());
+    expect(modelWithAllFieldTypes.stringMapFieldEmbedded).toEqual(new Map<string, generated.EmptyModel>());
+    modelWithAllFieldTypes.stringArrayField.push('hello', 'world');
+    modelWithAllFieldTypes.stringMapField.set('hello', 'world');
+    modelWithAllFieldTypes.stringMapFieldEmbedded.set('hello', emptyModel);
+
+    expect(modelWithAllFieldTypes.int32Field).toEqual(32);
+    modelWithAllFieldTypes.int32Field = 42;
+    expect(modelWithAllFieldTypes.int32ArrayField.length).toEqual(0);
+    expect(modelWithAllFieldTypes.int32MapField).toEqual(new Map<number, number>());
+    expect(modelWithAllFieldTypes.int32MapFieldEmbedded).toEqual(new Map<number, generated.EmptyModel>());
+    modelWithAllFieldTypes.int32ArrayField.push(42, 84);
+    modelWithAllFieldTypes.int32MapField.set(42, 84);
+    modelWithAllFieldTypes.int32MapFieldEmbedded.set(42, emptyModel);
+
+    expect(modelWithAllFieldTypes.int64Field).toEqual(BigInt(64));
+    modelWithAllFieldTypes.int64Field = BigInt(100);
+    expect(modelWithAllFieldTypes.int64ArrayField.length).toEqual(0);
+    expect(modelWithAllFieldTypes.int64MapField).toEqual(new Map<bigint, bigint>());
+    expect(modelWithAllFieldTypes.int64MapFieldEmbedded).toEqual(new Map<bigint, generated.EmptyModel>());
+    modelWithAllFieldTypes.int64ArrayField.push(BigInt(100), BigInt(200));
+    modelWithAllFieldTypes.int64MapField.set(BigInt(100), BigInt(200));
+    modelWithAllFieldTypes.int64MapFieldEmbedded.set(BigInt(100), emptyModel);
 });
 
 test('test-input', () => {
